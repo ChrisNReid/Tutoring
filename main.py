e70c30a0b044e2c4a8eb13b5e61c86456c506d9f
Christopher Reid <66421358+ChrisNReid@users.noreply.github.com>
2022-11-09 19:15:44 +0000

    hangman start

--- main.py
+++ main.py
@@ -1,39 +1,24 @@
-#create a program that will keep track of items for a shopping list. The program should allow you to keep adding new items. You should also be able to record which shop you are going to visit for the item
+#Hangman
+# Create a version of the Hangman game using Lists. The program should ask for the word to guess and the number of chances to be given
+#For every guess you should update the user with which letters they have guessed incorrectly, as well as replacing the letters in the guess word with the ones they have guessed correctly.
+#You should also show the user how many chances they have left.
 
-#ask the user for an item
-#ask user for items shop
-#add both to a text file
+word=input("Enter the word to guess: ")
+chances=input("Enter how many chances should be given: ")
 
+guess = []
+for i in range(len(word)):
+  guess.append("_ ")
 
-item=input("Enter item :")
-shop=input("Enter item's shop :")
-priority=input("Enter the priority of the item :")
-
-f =  open('shoppingList.txt','a')
-f.write("\n" + item + " - " + shop +" - "+ priority)
-f.close()
-
-print("reading")
-f = open('shoppingList.txt','r')
-print(f.read()) 
-
-
-#to order
-#put in a 2d array
-# [Grapes - Tesco - 1]
-# [Apples - Tesco - 5]
-# [Tomato - Waitrose - 2]
-# [Banana - Tesco - 3]
-# [Strawberry - Aldi - 4]
-
-# sort based on the number at the end
-# x = len(i)
-# i[x]
-# i[-1] = goes to the end of a list/string etc
-# -1 means the end
-
-x= "hello world"
-print(x[-1])
-
-
-
+print(guess)
+count = 0
+usrguess = ""
+while count <= 10 and usrguess != word:
+  #enter user guess
+  #if guess correct
+    #add letter to guess array
+  # if not
+    #tell them incorrect
+  
+  #count increases
+  
