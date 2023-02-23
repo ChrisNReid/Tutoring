f25bc4f76af92d571bf5fbf95f24518f88880d0f
Christopher Reid <66421358+ChrisNReid@users.noreply.github.com>
2023-02-23 18:15:49 +0000

    combinations

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 number1=input("Enter a number :")
 number2=input("Enter a second number :")
 number3=input("Enter a third number :")
-number4=input("Enter a fourth number :")
+# number4=input("Enter a fourth number :")
 
 array=[]
 array.append(number1)
@@ -15,11 +15,8 @@ array.append(number3)
 for i in range(len(array)):
   for j in range(len(array)):
     for k in range(len(array)):
-        if (array[i] == array[j])  and (array[j]==array[k]) and (array[i]==array[k]):
-          x=4
-        else:
+        if i!=j and i!=k and j!=k:
           print(array[i], array[j], array[k])
-
 
 #scale up
 #make it a function that accepts a list
